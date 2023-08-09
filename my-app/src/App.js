7f91958c0a12545fd2b998d034de6133682b468f
Prathyarthi <Prathyarthi@prathyarthi.com>
2023-08-09 22:57:53 +0530

    Succesfully added Enable DarkMode button

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,26 @@
 import Navbar from './components/Navbar';
 import './App.css';
 import TextForm from './components/TextForm';
+import { useState } from 'react';
 
 function App() {
+  const [mode, setMode] = useState('light');
+
+  const toggleMode = () => {
+    if (mode === 'light') {
+      setMode('dark');
+      document.body.style.backgroundColor = 'black';
+    }
+    else {
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+    }
+  }
   return (
     <>
-      <Navbar heading="TextUtils"/>
+      <Navbar heading="TextUtils" mode={mode} toggleMode={toggleMode} />
       <div className="container my-3">
-        <TextForm heading="Enter your text here :) "/>
+        <TextForm heading="Enter your text here :) " mode={mode}/>
       </div>
     </>
   );
--- my-app/src/components/TextForm.js
+++ my-app/src/components/TextForm.js
@@ -21,22 +21,30 @@ export default function TextForm(props) {
         setText(newText)
     }
 
+    const handleCopy = () => {
+        var text = document.getElementById("my-box");
+        text.select();
+        navigator.clipboard.writeText(text.value);
+    }
+
     const [text, setText] = useState("");
 
     return (
         <>
-            <h1>{props.heading}</h1>
-            <div className="mb-3">
+            <div className="mb-3" style={{ color: props.mode === 'dark' ? 'white' : 'black' }}>
+                <h1>{props.heading}</h1>
                 <textarea
+                    style={{ backgroundColor: props.mode === 'dark' ? 'black' : 'white', color: props.mode === 'dark' ? 'white' : 'black' }}
                     onChange={handleOnChange}
                     value={text}
                     className="form-control"
-                    id="exampleFormControlTextarea1"
+                    id="my-box"
                     rows={3}
                 />
                 <button className="btn btn-primary my-3 mx-2" onClick={handleOnClickUpper} >Convert to UPPERCASE</button>
                 <button className="btn btn-primary my-3 mx-2" onClick={handleOnClickLower} >Convert to lowercase</button>
                 <button className="btn btn-primary my-3 mx-2" onClick={handleClearText} >Clear Text</button>
+                <button className="btn btn-primary my-3 mx-2" onClick={handleCopy} >Copy Text</button>
                 <div>
                     <p>{"Number of words : " + text.split(" ").length}</p>
                     <p>{"Number of characters : " + text.length}</p>
